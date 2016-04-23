847dc2cd759a8436d9fd409fe8e4258ca9ba8932
Kampfeer <kaizerdragon5@hotmail.com>
2016-04-23 10:07:32 -0400

    Communication with Database
    Was able to establish a connection with the database few days ago after
    fighting with dependencies and establishing connections when not running
    the website from main().  Will work on pulling info from database for
    both instructor and student site, clean up the layout of both sites, and
    making a class to handle communication with the database.

--- Project Management Assessment/src/main/java/com/softwareengineers/web/controller/instructor/InstructorController.java
+++ Project Management Assessment/src/main/java/com/softwareengineers/web/controller/instructor/InstructorController.java
@@ -19,23 +19,19 @@ public class InstructorController {
     @RequestMapping(value="/instructor/show", method=RequestMethod.GET)
     public ModelAndView login(HttpServletRequest request) {
         Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request);
-        if((Boolean)flashMap.get("isAdmin")){
-            Map<String,String> model = new HashMap<String,String>();
-            model.put("username", (String)flashMap.get("username"));
+        Map<String,String> model = new HashMap<String,String>();
+        model.put("username", (String)flashMap.get("username"));
             
-            //THESE WILL BE PULLED FROM THE DATABASE, HARD CODING THEM FOR NOW TO GET THE FRONT END WORKING
-            //To get these in the jsp we will use tokens
-            //For example, the class array will be split with just commas (no whitespace between them) for each class
-            //The assessments with have each assessment separated by comma (no whitespace) and then a slash as a separater for when
-            //we have reached the assessments for the next class
-            String classArray = "Project Management,Software Engineering";
-            model.put("classes", classArray);
-            model.put("assessments", "peer,self");
+        //THESE WILL BE PULLED FROM THE DATABASE, HARD CODING THEM FOR NOW TO GET THE FRONT END WORKING
+        //To get these in the jsp we will use tokens
+        //For example, the class array will be split with just commas (no whitespace between them) for each class
+        //The assessments with have each assessment separated by comma (no whitespace) and then a slash as a separater for when
+        //we have reached the assessments for the next class
+        String classArray = "Project Management,Software Engineering";
+        model.put("classes", classArray);
+        model.put("assessments", "peer,self");
             
-            return new ModelAndView("instructorSite", "model", model);
-        }
-        else
-            return new ModelAndView("error");
+        return new ModelAndView("instructorSite", "model", model);
     }
     
     @RequestMapping(value="/instructorTabContainer")
